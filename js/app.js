9b31750284d838fb555880ae1f3d3b48c3d9450c
Silvia Roig <silviaroigc@gmail.com>
2019-06-07 20:37:43 +0200

    WIP

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function showOverlay(wrapperId) {
+    document.getElementById(wrapperId).classList.add('active');
+}
+function hideOverlay(wrapperId) {
+    document.getElementById(wrapperId).classList.remove('active');
+}
+
+window.onload = function() {
+    
+}
