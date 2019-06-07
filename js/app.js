275add77650698c711186df8b243335778f8fde5
Salva Camarasa <salvacms@gmail.com>
2019-06-07 22:15:27 +0200

    Add animations and magic :)

--- js/app.js
+++ js/app.js
@@ -5,17 +5,60 @@ function hideOverlay(wrapperId) {
     document.getElementById(wrapperId).classList.remove('active');
 }
 
-window.onload = function() {
+$(document).ready(function() {
+
+    //Show / hide scoll information container
     var scrollContainer = document.getElementById('scroll-container');
 
-    document.addEventListener('scroll', function() {
-        var offsetHeight = window.scrollY;
+    if (scrollContainer) {
+        document.addEventListener('scroll', function() {
+            var offsetHeight = window.scrollY;
+    
+            if (offsetHeight > 12) {
+                scrollContainer.classList.add('hidden');
+            }
+            else {
+                scrollContainer.classList.remove('hidden');
+            }
+        });
+    }
 
-        if (offsetHeight > 12) {
-            scrollContainer.classList.add('hidden');
-        }
-        else {
-            scrollContainer.classList.remove('hidden');
-        }
+    //Scroll from menu to location handler
+    var linkProjects = document.getElementById('link-projects');
+    var projectStartElement = document.getElementById('project-start');
+
+    if (linkProjects && projectStartElement) {
+        linkProjects.addEventListener('click', function(event) {
+            event.preventDefault();
+    
+            $('html, body').animate({
+                scrollTop: $(projectStartElement).offset().top
+            }, 200);
+        });
+    }    
+
+    //Sidebar menu scroll to location handler
+    $('aside.sidebar-menu a').on('click', function(e) {
+        e.preventDefault();
+
+        var destination = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(destination).offset().top
+        }, 300);
     });
-}
+
+    //Sidebar menu show/hide on scroll
+    if ($('.sidebar-menu').length && window.innerWidth > 1200) {       
+        $(document).on('scroll', function() {
+            var offsetHeight = window.scrollY;
+
+            if (offsetHeight > $('#main-title').offset().top - 100) {
+                $('.sidebar-menu').removeClass('hidden');
+            }
+            else {
+                $('.sidebar-menu').addClass('hidden');
+            }
+        });
+    }
+});
